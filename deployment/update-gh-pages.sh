710709918d9f0730bb194e417900af8898236714
Andjelko Perisic <andjelko.perisic@gmail.com>
2021-09-16 20:14:02 +0200

    debug output

--- deployment/update-gh-pages.sh
+++ deployment/update-gh-pages.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+repo_uri="https://x-access-token:$DEPLOY_TOKEN@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
 main_branch="main"
 target_branch="gh-pages"
