164eb83fcd82fb2dcb6625fdbf66c52d9f489fef
Andjelko Perisic <andjelko.perisic@gmail.com>
2021-09-23 18:59:25 +0200

    icon registry refactored

---
+++ src/app/icon-registry.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconRegistryService {
+  private githubUrl: string = 'assets/icons/github.svg';
+  private stackoverflowUrl: string = 'assets/icons/stackoverflow.svg';
+  private linkedinUrl: string = 'assets/icons/linkedin.svg';
+  private xingUrl: string = 'assets/icons/xing.svg';
+  private twitterUrl: string = 'assets/icons/twitter.svg';
+
+  constructor(
+    private domSanitizer: DomSanitizer,
+    public matIconRegistry: MatIconRegistry
+  ) {}
+
+  public initilizeIcons(): void {
+    this.matIconRegistry
+      .addSvgIcon(
+        'github-dark',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(this.githubUrl)
+      )
+      .addSvgIcon(
+        'stackoverflow-dark',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(this.stackoverflowUrl)
+      )
+      .addSvgIcon(
+        'linkedin-dark',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(this.linkedinUrl)
+      )
+      .addSvgIcon(
+        'xing-dark',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(this.xingUrl)
+      )
+      .addSvgIcon(
+        'twitter-dark',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(this.twitterUrl)
+      );
+  }
+}
